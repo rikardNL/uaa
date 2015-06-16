35a1cf1e7e3ad9e6795842aa6fe44b33e57ff41e
Rikard Andersson <rikard.andersson@netlight.com>
2015-06-16 11:14:51 +0200

    Made cert generation play without sun security libs, using bouncycastle instead (also java 8 compatibility).

--- uaa/src/test/java/org/springframework/security/ldap/server/ApacheDsSSLContainer.java
+++ uaa/src/test/java/org/springframework/security/ldap/server/ApacheDsSSLContainer.java
@@ -1,19 +1,25 @@
 package org.springframework.security.ldap.server;
 
-
 import org.apache.directory.server.ldap.LdapServer;
 import org.apache.directory.server.ldap.handlers.extended.StartTlsHandler;
 import org.apache.directory.server.protocol.shared.transport.TcpTransport;
-import sun.security.x509.CertAndKeyGen;
-import sun.security.x509.X500Name;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.math.BigInteger;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.KeyStore;
-import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
 import java.util.Date;
+
+import javax.security.auth.x500.X500Principal;
 
 public class ApacheDsSSLContainer extends ApacheDSContainer {
     private int port = 53389;
@@ -102,13 +108,32 @@ public class ApacheDsSSLContainer extends ApacheDSContainer {
 
         KeyStore keyStore = KeyStore.getInstance("JKS");
         keyStore.load(null, null);
-        CertAndKeyGen keypair = new CertAndKeyGen("RSA", "SHA1WithRSA", null);
-        X500Name x500Name = new X500Name(commonName, organizationalUnit, organization, city, state, country);
-        keypair.generate(keysize);
-        PrivateKey privKey = keypair.getPrivateKey();
-        X509Certificate[] chain = new X509Certificate[1];
-        chain[0] = keypair.getSelfCertificate(x500Name, new Date(), (long) validity * 24 * 60 * 60);
-        keyStore.setKeyEntry(alias, privKey, keyPass, chain);
+
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+        keyPairGen.initialize(keysize);
+        KeyPair keyPair = keyPairGen.generateKeyPair();
+
+        X500Principal x500Principal = new X500Principal(
+            String.format(
+                "CN=%s, OU=%s, O=%s, L=%s, ST=%s, C=%s",
+                commonName, organizationalUnit, organization, city, state, country));
+
+        ContentSigner signer = new JcaContentSignerBuilder("SHA1withRSA").setProvider("BC").build(keyPair.getPrivate());
+
+        X509CertificateHolder certHolder = new JcaX509v3CertificateBuilder(
+            x500Principal,
+            BigInteger.ONE,
+            new Date(System.currentTimeMillis() - 24 * 60 * 60 * 1000),
+            new Date(System.currentTimeMillis() + 365 * 60 * 60 * 1000),
+            x500Principal,
+            keyPair.getPublic()
+        ).build(signer);
+
+        X509Certificate[] certChain = new X509Certificate[1];
+        certChain[0] = new JcaX509CertificateConverter().getCertificate(certHolder);
+
+        keyStore.setKeyEntry(alias, keyPair.getPrivate(), keyPass, certChain);
+
         String keystoreName = "ldap.keystore";
         File keystore = new File(directory, keystoreName);
         if (!keystore.createNewFile()) {
